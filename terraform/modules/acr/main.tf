f63a8d649ce7ccbe77ad02ebf37820601862b173
mfenerich <marcelfenerich1@gmail.com>
2025-02-17 10:14:45 +0100

    Refactor Azure resources: update container registry name format and adjust resource group lifecycle settings; comment out virtual network resource

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "this" {
-  name                = "${var.environment}acr${var.suffix}" # TODO: Change to acr-${var.environment}-${var.suffix}
+  name                = "${var.environment}acr${var.suffix}"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = var.sku
--- terraform/modules/resource_groups/main.tf
+++ terraform/modules/resource_groups/main.tf
@@ -4,6 +4,6 @@ resource "azurerm_resource_group" "this" {
   tags     = merge(var.tags, { environment = var.environment })
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = var.environment == "prod" ? true : false
   }
 }
