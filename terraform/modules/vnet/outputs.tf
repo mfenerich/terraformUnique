a0f411bdb11de3eb20908516f18f078e53847538
mfenerich <marcelfenerich1@gmail.com>
2025-02-17 06:36:57 +0100

    Comment out cosmosdb_subnet_id output in VNet module

--- terraform/modules/vnet/outputs.tf
+++ terraform/modules/vnet/outputs.tf
@@ -6,6 +6,6 @@ output "aks_subnet_id" {
   value = azurerm_subnet.aks.id
 }
 
-output "cosmosdb_subnet_id" {
-  value = azurerm_subnet.cosmosdb.id
-}
+# output "cosmosdb_subnet_id" {
+#   value = azurerm_subnet.cosmosdb.id
+# }
