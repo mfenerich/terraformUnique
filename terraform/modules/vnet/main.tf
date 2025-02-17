1c9d18a8839cac7a7bdc35005ead18856e107292
mfenerich <marcelfenerich1@gmail.com>
2025-02-17 06:34:20 +0100

    Remove Helm release module and associated variables from Terraform configuration

--- terraform/modules/vnet/main.tf
+++ terraform/modules/vnet/main.tf
@@ -14,9 +14,9 @@ resource "azurerm_subnet" "aks" {
   service_endpoints    = ["Microsoft.AzureCosmosDB"]
 }
 
-resource "azurerm_subnet" "cosmosdb" {
-  name                 = "cosmosdb-subnet-${var.environment}-${var.suffix}"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_subnet" "cosmosdb" {
+#   name                 = "cosmosdb-subnet-${var.environment}-${var.suffix}"
+#   resource_group_name  = var.resource_group_name
+#   virtual_network_name = azurerm_virtual_network.this.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
